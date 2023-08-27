bd1f45b43701f5656d85b1043aeb6f03f57f5877
Nigel Dsouza <nigel10122@gmail.com>
2023-08-27 00:59:09 -0500

    added dummy keywords

--- src/index.js
+++ src/index.js
@@ -21,9 +21,26 @@ nigiClient.on('ready', (e) => {
 
 nigiClient.on('messageCreate', async (message) => {
 
-        if (!message.author.bot && message.content.includes("hey pranit")){
+        if (!message.author.bot &&  includesKeyword(message)){
             message.reply(await nigiBot(process.env.OPEN_AI_TOKEN, message.content));
         }
      
 });
+
+
+const includesKeyword = async (message) => {
+
+
+    const keyWords = ['hey pranit', 'eeh smiith', 'buddy', 'hoohoo', 'yaar'];
+
+    let includesKeyWord = true;
+
+    for(const keyword of keyWords){
+        if(!message.content.includes(keyword)){
+                includesKeyWord = false;
+        }   
+    }
+
+    return includesKeyWord;
+}
 
